6fcf68004d43396fc40a7c345b32cd6e100d5587
hisadomi <hb.kmaia93@icloud.com>
2020-11-11 08:02:48 +0900

    Twitterログイン、ログアウト機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/auth/:provider/callback', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+  root to: "users#index"
 end
