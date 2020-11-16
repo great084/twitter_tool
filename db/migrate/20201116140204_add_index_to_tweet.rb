f0c98c0d87e88978535019de7dba06741fc0f7d9
nozomi <kkk@docomo.ne.jp>
2020-11-16 23:27:28 +0900

    Tweetモデルにインデックスを追加

--- db/migrate/20201116140204_add_index_to_tweet.rb
+++ db/migrate/20201116140204_add_index_to_tweet.rb
@@ -1,5 +1,10 @@
 class AddIndexToTweet < ActiveRecord::Migration[6.0]
   def change
-    add_index :tweets, [:tweet_created_at, :text,:retweet_count,:favorite_count,:tweet_flag,:retweet_flag]
+    add_index :tweets,:tweet_created_at
+    add_index :tweets,:text
+    add_index :tweets,:retweet_count
+    add_index :tweets,:favorite_count
+    add_index :tweets,:tweet_flag
+    add_index :tweets,:retweet_flag
   end
 end
