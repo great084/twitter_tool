c0a99a0d92762cdc4435e5707400d5d85a0cea3c
nozomi <kkk@docomo.ne.jp>
2020-11-19 08:47:51 +0900

    textカラムのインデックスを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_16_140204) do
+ActiveRecord::Schema.define(version: 2020_11_18_234531) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,6 @@ ActiveRecord::Schema.define(version: 2020_11_16_140204) do
     t.index ["favorite_count"], name: "index_tweets_on_favorite_count"
     t.index ["retweet_count"], name: "index_tweets_on_retweet_count"
     t.index ["retweet_flag"], name: "index_tweets_on_retweet_flag"
-    t.index ["text"], name: "index_tweets_on_text"
     t.index ["tweet_created_at"], name: "index_tweets_on_tweet_created_at"
     t.index ["tweet_flag"], name: "index_tweets_on_tweet_flag"
     t.index ["user_id"], name: "index_tweets_on_user_id"
