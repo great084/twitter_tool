6ca09f7747e6ef84761d9d33a8373731836877eb
nozomi <kkk@docomo.ne.jp>
2020-11-22 13:23:21 +0900

    ページネーションを実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,7 @@
 class TweetsController < ApplicationController
+  PER_PAGE = 10
   def index
-    @tweets=Tweet.order(id: :desc).includes(:media)
-
+    @tweets=Tweet.order(id: :desc).includes(:media).page(params[:page]).per(PER_PAGE)
   end
 
 
