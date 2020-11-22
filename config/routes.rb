374f8aa608b706bdc29277043db96c6c695c3d9d
great084 <great084@gmail.com>
2020-11-22 21:47:44 +0900

    twtterログインのrspec作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/auth/:provider/callback', to: 'sessions#create' #ログイン認証
+  get '/logout', to: 'sessions#destroy' #ログアウト用
+  get "/auth/failure", to: "sessions#failure" #認証失敗時用
+  root to: "users#index"
 end
