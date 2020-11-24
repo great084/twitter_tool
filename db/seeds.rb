ed1cd61a04cca4b2cc7a1bbca99424f2dc79983a
nozomi <kkk@docomo.ne.jp>
2020-11-25 08:51:00 +0900

    seedデータを再投入

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,16 @@
+# User.create!(
+#   uid:"123456789",
+#   nickname:"test"
+
+# )
 10.times do |n|
   Tweet.create!(
     tweet_created_at: "Sat, 21 Nov 2020 02:28:09 UTC +00:00",
     tweet_id:"123456789",
     text: "こんにちは#{n + 1}",
-    retweet_count: 1,
-    favorite_count: 1,
-    user_id: 2,
+    retweet_count: n+1,
+    favorite_count: n+1,
+    user_id: 1,
     tweet_flag: true,
     retweet_flag: false
   )
