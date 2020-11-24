6a7755bed3b64a84a6a825af444b7e9567c85140
nozomi <kkk@docomo.ne.jp>
2020-11-25 08:39:02 +0900

    boolean型カラムのnull false制約追加、バリテーション変更

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -5,5 +5,7 @@ class Tweet < ApplicationRecord
   validates :text, presence: true
   validates :retweet_count, presence: true
   validates :favorite_count, presence: true
+  validates :retweeet_flag, inclusion: [true, false]
+  validates :tweet_flag, inclusion: [true, false]
   has_many :media, dependent: :destroy
 end
