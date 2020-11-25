d3f536da4b19d909375a17558ebf2e3fc69d1e27
great084 <great084@gmail.com>
2020-11-25 09:57:07 +0900

    rubocopの設定追加＆自動修正実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-   validates :uid, presence: true
-   validates :nickname, presence: true
-   has_many :tweets, dependent: :destroy
+  validates :uid, presence: true
+  validates :nickname, presence: true
+  has_many :tweets, dependent: :destroy
 end
--- db/migrate/20201116003315_create_tweets.rb
+++ db/migrate/20201116003315_create_tweets.rb
@@ -7,8 +7,8 @@ class CreateTweets < ActiveRecord::Migration[6.0]
       t.integer :retweet_count, null: false
       t.integer :favorite_count, null: false
       t.references :user, null: false, foreign_key: true
-      t.boolean :tweet_flag,default: false, null: false
-      t.boolean :retweet_flag,default: false, null: false
+      t.boolean :tweet_flag, default: false, null: false
+      t.boolean :retweet_flag, default: false, null: false
 
       t.timestamps
     end
