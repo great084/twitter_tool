dc1e5689bbce30045197da77299d56a30e167c17
hisadomi <hb.kmaia93@icloud.com>
2020-11-25 22:28:23 +0900

    Tweetモデルへのデータ格納実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,44 +1,22 @@
-
-
 class TweetsController < ApplicationController
   require 'net/http'
   require 'uri'
   require 'json'
+  include SessionsHelper
 
   def index
-    client = twitter_client
-    @user = User.first
-
-    #Twitter developerのコード
-    uri = URI.parse("https://api.twitter.com/1.1/tweets/search/30day/dev.json")
-
-    request = Net::HTTP::Post.new(uri)
-    request["Authorization"] = "Bearer #{ENV["BEARER_TOKEN"]}"
-    request.body = "{
-      \"query\":\"from:YUTAJAPAN210\",
-      \"maxResults\":\"10\",
-      \"fromDate\":\"202010201200\",
-      \"toDate\":\"202010262000\"
-    }"
-
-    req_options = {
-      use_ssl: uri.scheme == "https",
-    }
-
-    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
-      http.request(request)
+    tweet = Tweet.new()
+    response = tweet.twitter_search_data
+    response["results"].each do |res|
+      Tweet.create!(
+        user_id: current_user.id,
+        tweet_created_at: res["created_at"],
+        tweet_id: res["id_str"],
+        text: res["text"],
+        retweet_count: res["retweet_count"],
+        favorite_count: res["favorite_count"]
+      )
     end
-    @response = JSON.parse(response.body)
-    binding.pry
-    
-  end
-
-  def twitter_client
-    return client = Twitter::REST::Client.new do |config|
-      config.consumer_key = ENV["TWITTER_API_KEY"]
-      config.consumer_secret = ENV["TWITTER_API_SECRET"]
-      config.access_token = ENV["ACCESS_TOKEN"]
-      config.access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
-    end
+    @tweets = Tweet.all
   end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -5,7 +5,51 @@ class Tweet < ApplicationRecord
   validates :text, presence: true
   validates :retweet_count, presence: true
   validates :favorite_count, presence: true
-  validates :tweet_flag, presence: true
-  validates :retweet_flag, presence: true
+  validates :tweet_flag, inclusion: [true, false]
+  validates :retweet_flag, inclusion: [true, false]
   has_many :media, dependent: :destroy
+
+
+  def twitter_search_data
+    client = twitter_client
+    #Twitter developerのコード
+    uri = URI.parse("https://api.twitter.com/1.1/tweets/search/30day/dev.json")
+
+    request = Net::HTTP::Post.new(uri)
+    request["Authorization"] = "Bearer #{ENV["BEARER_TOKEN"]}"
+    request.body = fetch_request_body
+    req_options = {
+      use_ssl: uri.scheme == "https",
+    }
+
+    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+      http.request(request)
+    end
+    @response = JSON.parse(response.body)
+  end
+
+
+  def twitter_client
+    return client = Twitter::REST::Client.new do |config|
+      config.consumer_key = ENV["TWITTER_API_KEY"]
+      config.consumer_secret = ENV["TWITTER_API_SECRET"]
+      config.access_token = ENV["ACCESS_TOKEN"]
+      config.access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
+    end
+  end
+
+  def fetch_request
+
+  end
+
+  #クエリ指定
+  def fetch_request_body
+    body = "{
+      \"query\":\"from:#{ENV["TWEET_USER"]}\",
+      \"maxResults\":\"10\",
+      \"fromDate\":\"202010281200\",
+      \"toDate\":\"202011042000\"
+    }"
+  end
+
 end
