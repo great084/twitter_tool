e5d3ee43edae97212bb30d5f384258687030bab7
nozomi <kkk@docomo.ne.jp>
2020-11-26 09:58:21 +0900

    一覧ページ、詳細ページレイアウト調整

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,5 @@
 class TweetsController < ApplicationController
-  PER_PAGE =5
+  PER_PAGE =10
   def index
     @tweets=Tweet.order(id: :desc).includes(:media).page(params[:page]).per(PER_PAGE)
   end
