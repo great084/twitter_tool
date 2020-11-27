4cda2c73423c09d830c8d473ac842dd78cbba59a
hisadomi <hb.kmaia93@icloud.com>
2020-11-28 08:30:40 +0900

    現remoteのマスターブランチをマージ

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/auth/:provider/callback', to: 'sessions#create'
+  get '/auth/failure', to: 'sessions#failure'
   get '/logout', to: 'sessions#destroy'
   resources :tweets, only: [:index]
   root to: "users#index"
