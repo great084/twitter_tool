8e5a19ffd785a1703396a2f40f57af267ed83955
hisadomi <hb.kmaia93@icloud.com>
2020-11-28 08:28:57 +0900

    ベアラートークン使用のため、アクセストークン 使用の記述削除

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -28,8 +28,6 @@ class Tweet < ApplicationRecord
     return client = Twitter::REST::Client.new do |config|
       config.consumer_key = ENV["TWITTER_API_KEY"]
       config.consumer_secret = ENV["TWITTER_API_SECRET"]
-      config.access_token = ENV["ACCESS_TOKEN"]
-      config.access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
     end
   end
 
