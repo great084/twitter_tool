1aaea08860d0f01a7951d4b1d29a4f7b6854c286
great084 <great084@gmail.com>
2020-11-28 20:11:23 +0900

    omniauth_mock.rbに対するrubocopのインデント適用を除外

--- spec/support/omniauth_mocks.rb
+++ spec/support/omniauth_mocks.rb
@@ -1,95 +1,95 @@
 module OmniauthMocks
   def twitter_mock_uid_234567
     OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new({
-                                                                   'provider' => 'twitter',
-                                                                   'uid' => '234567',
-                                                                   'info' => {
-                                                                     'nickname' => 'mockmock',
-                                                                     'name' => 'Mock User',
-                                                                     'image' => 'http://mock_image_url.com',
-                                                                     'location' => '',
-                                                                     'email' => 'mock@example.com',
-                                                                     'urls' => {
-                                                                       'Twitter' => 'https://twitter.com/MockUser1234',
-                                                                       'Website' => ''
-                                                                     }
-                                                                   },
-                                                                   'credentials' => {
-                                                                     'token' => 'mock_credentails_token',
-                                                                     'secret' => 'mock_credentails_secret'
-                                                                   },
-                                                                   'extra' => {
-                                                                     'raw_info' => {
-                                                                       'name' => 'Mock User',
-                                                                       'id' => '123456',
-                                                                       'followers_count' => 0,
-                                                                       'friends_count' => 0,
-                                                                       'statuses_count' => 0
-                                                                     }
-                                                                   }
-                                                                 })
+      'provider' => 'twitter',
+      'uid' => '234567',
+      'info' => {
+        'nickname' => 'mockmock',
+        'name' => 'Mock User',
+        'image' => 'http://mock_image_url.com',
+        'location' => '',
+        'email' => 'mock@example.com',
+        'urls' => {
+          'Twitter' => 'https://twitter.com/MockUser1234',
+          'Website' => ''
+        }
+      },
+      'credentials' => {
+        'token' => 'mock_credentails_token',
+        'secret' => 'mock_credentails_secret'
+      },
+      'extra' => {
+        'raw_info' => {
+          'name' => 'Mock User',
+          'id' => '123456',
+          'followers_count' => 0,
+          'friends_count' => 0,
+          'statuses_count' => 0
+        }
+      }
+    })
   end
 
   def twitter_mock_uid_123456
     OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new({
-                                                                   'provider' => 'twitter',
-                                                                   'uid' => '123456',
-                                                                   'info' => {
-                                                                     'nickname' => 'mockmock',
-                                                                     'name' => 'Mock User',
-                                                                     'image' => 'http://mock_image_url.com',
-                                                                     'location' => '',
-                                                                     'email' => 'mock@example.com',
-                                                                     'urls' => {
-                                                                       'Twitter' => 'https://twitter.com/MockUser1234',
-                                                                       'Website' => ''
-                                                                     }
-                                                                   },
-                                                                   'credentials' => {
-                                                                     'token' => 'mock_credentails_token',
-                                                                     'secret' => 'mock_credentails_secret'
-                                                                   },
-                                                                   'extra' => {
-                                                                     'raw_info' => {
-                                                                       'name' => 'Mock User',
-                                                                       'id' => '123456',
-                                                                       'followers_count' => 0,
-                                                                       'friends_count' => 0,
-                                                                       'statuses_count' => 0
-                                                                     }
-                                                                   }
-                                                                 })
+      'provider' => 'twitter',
+      'uid' => '123456',
+      'info' => {
+        'nickname' => 'mockmock',
+        'name' => 'Mock User',
+        'image' => 'http://mock_image_url.com',
+        'location' => '',
+        'email' => 'mock@example.com',
+        'urls' => {
+          'Twitter' => 'https://twitter.com/MockUser1234',
+          'Website' => ''
+        }
+      },
+      'credentials' => {
+        'token' => 'mock_credentails_token',
+        'secret' => 'mock_credentails_secret'
+      },
+      'extra' => {
+        'raw_info' => {
+          'name' => 'Mock User',
+          'id' => '123456',
+          'followers_count' => 0,
+          'friends_count' => 0,
+          'statuses_count' => 0
+        }
+      }
+    })
   end
 
   # 異常なデータ(uidが欠落)
   def twitter_invalid_mock
     OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new({
-                                                                   'provider' => 'twitter',
-                                                                   # "uid" => "123456",
-                                                                   'info' => {
-                                                                     'name' => 'Mock User',
-                                                                     'image' => 'http://mock_image_url.com',
-                                                                     'location' => '',
-                                                                     'email' => 'mock@example.com',
-                                                                     'urls' => {
-                                                                       'Twitter' => 'https://twitter.com/MockUser1234',
-                                                                       'Website' => ''
-                                                                     }
-                                                                   },
-                                                                   'credentials' => {
-                                                                     'token' => 'mock_credentails_token',
-                                                                     'secret' => 'mock_credentails_secret'
-                                                                   },
-                                                                   'extra' => {
-                                                                     'raw_info' => {
-                                                                       'name' => 'Mock User',
-                                                                       'id' => '123456',
-                                                                       'followers_count' => 0,
-                                                                       'friends_count' => 0,
-                                                                       'statuses_count' => 0
-                                                                     }
-                                                                   }
-                                                                 })
+      'provider' => 'twitter',
+      # "uid" => "123456",
+      'info' => {
+        'name' => 'Mock User',
+        'image' => 'http://mock_image_url.com',
+        'location' => '',
+        'email' => 'mock@example.com',
+        'urls' => {
+          'Twitter' => 'https://twitter.com/MockUser1234',
+          'Website' => ''
+        }
+      },
+      'credentials' => {
+        'token' => 'mock_credentails_token',
+        'secret' => 'mock_credentails_secret'
+      },
+      'extra' => {
+        'raw_info' => {
+          'name' => 'Mock User',
+          'id' => '123456',
+          'followers_count' => 0,
+          'friends_count' => 0,
+          'statuses_count' => 0
+        }
+      }
+    })
   end
 
   def twitter_cancel_mock
