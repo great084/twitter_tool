a5f6dff94c60fb49571e1ebdf5961a96527fcbd3
nozomi <kkk@docomo.ne.jp>
2020-11-30 12:52:02 +0900

    ログインユーザーのツイートのみを一覧表示

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,10 +1,16 @@
 class TweetsController < ApplicationController
+  include SessionsHelper
+  before_action :tweet_user
   PER_PAGE =10
   def index
-    @tweets=Tweet.order(id: :desc).includes(:media).page(params[:page]).per(PER_PAGE)
+    @tweets=Tweet.where(user_id: @user.id).order(id: :desc).includes(:media).page(params[:page]).per(PER_PAGE)
   end
   def show
    @tweet = Tweet.find(params[:id])
   end
 
+  private
+  def tweet_user
+    @user=current_user
+  end
 end
