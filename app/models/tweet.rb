5459a9ffa761aa8f22767c61f7fe6cda2cdef67d
hisadomi <hb.kmaia93@icloud.com>
2020-12-06 00:42:03 +0900

    rubocopにて指摘箇所修正

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -11,52 +11,53 @@ class Tweet < ApplicationRecord
 
   class << self
     def twitter_search_data(query_params)
-      uri = URI.parse("https://api.twitter.com/1.1/tweets/search/#{ENV["PLAN"]}/#{ENV["LABEL"]}.json")
+      uri = URI.parse("https://api.twitter.com/1.1/tweets/search/#{ENV['PLAN']}/#{ENV['LABEL']}.json")
       req_options = {
-        use_ssl: uri.scheme == "https",
+        use_ssl: uri.scheme == 'https'
       }
       response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
         http.request(fetch_request_data(uri, query_params))
       end
-      return response = JSON.parse(response.body)
+      JSON.parse(response.body)
     end
 
     def fetch_request_data(uri, query_params)
       request = Net::HTTP::Post.new(uri)
-      request["Authorization"] = "Bearer #{ENV["BEARER_TOKEN"]}"
+      request['Authorization'] = "Bearer #{ENV['BEARER_TOKEN']}"
       request.body = fetch_request_query(query_params)
       request
     end
 
-    #apiに送るクエリの取得
+    # apiに送るクエリの取得
     def fetch_query_params(form_params)
-      date_query = set_period_params(form_params[:period])
+      date_query = writer_period_params(form_params[:period])
       query_params = form_params.merge!(date_query)
-      query_params = query_params.merge!( {:next => nil} )
+      query_params.merge!({ next: nil })
     end
 
-    def set_period_params(period)
+    def writer_period_params(period)
       datetime = DateTime.now.gmtime
-      if period == "until_now"
-        params = {
-          date_to: datetime.ago(0.years).strftime("%Y%m%d%H%M"),
-          date_from: datetime.ago(1.years).strftime("%Y%m%d%H%M")
+      case period
+      when 'until_now' then
+        {
+          date_to: datetime.ago(0.years).strftime('%Y%m%d%H%M'),
+          date_from: datetime.ago(27.days).strftime('%Y%m%d%H%M')
         }
-      elsif period == "until_one_year"
-        params = {
-          date_to: datetime.ago(1.years).strftime("%Y%m%d%H%M"),
-          date_from: datetime.ago(10.years).strftime("%Y%m%d%H%M")
+      when 'until_one_year' then
+        {
+          date_to: datetime.ago(1.year).strftime('%Y%m%d%H%M'),
+          date_from: datetime.ago(10.years).strftime('%Y%m%d%H%M')
         }
       end
     end
-        
-    #クエリ指定
+
+    # クエリ指定
     def fetch_request_query(query_params)
-        body = "{
-          \"query\":\"from:#{query_params[:login_user]}\",
-          \"fromDate\":\"#{query_params[:date_from]}\",
-          \"toDate\":\"#{query_params[:date_to]}\"
-        }"      
+      "{
+        \"query\":\"from:#{query_params[:login_user]}\",
+        \"fromDate\":\"#{query_params[:date_from]}\",
+        \"toDate\":\"#{query_params[:date_to]}\"
+      }"
     end
   end
 end
