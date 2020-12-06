43de36675ecb75e8b18383c487c1c13d3d0e9246
great084 <great084@gmail.com>
2020-12-06 14:07:47 +0900

    rubocopの規約違反を解決

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -12,52 +12,53 @@ class Tweet < ApplicationRecord
 
   class << self
     def twitter_search_data(query_params)
-      uri = URI.parse("https://api.twitter.com/1.1/tweets/search/#{ENV["PLAN"]}/#{ENV["LABEL"]}.json")
+      uri = URI.parse("https://api.twitter.com/1.1/tweets/search/#{ENV['PLAN']}/#{ENV['LABEL']}.json")
       req_options = {
-        use_ssl: uri.scheme == "https",
+        use_ssl: uri.scheme == "https"
       }
       response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
         http.request(fetch_request_data(uri, query_params))
       end
-      return response = JSON.parse(response.body)
+      JSON.parse(response.body)
     end
 
     def fetch_request_data(uri, query_params)
       request = Net::HTTP::Post.new(uri)
-      request["Authorization"] = "Bearer #{ENV["BEARER_TOKEN"]}"
+      request["Authorization"] = "Bearer #{ENV['BEARER_TOKEN']}"
       request.body = fetch_request_query(query_params)
       request
     end
 
-    #apiに送るクエリの取得
+    # apiに送るクエリの取得
     def fetch_query_params(form_params)
-      date_query = set_period_params(form_params[:period])
+      date_query = period_params(form_params[:period])
       query_params = form_params.merge!(date_query)
-      query_params = query_params.merge!( {:next => nil} )
+      query_params.merge!({ next: nil })
     end
 
-    def set_period_params(period)
+    def period_params(period)
       datetime = DateTime.now
-      if period == "until_now"
-        params = {
+      case period
+      when "until_now"
+        {
           date_to: datetime.ago(9.hours).strftime("%Y%m%d%H%M"),
-          date_from: datetime.ago(1.years).strftime("%Y%m%d%H%M")
+          date_from: datetime.ago(1.year).strftime("%Y%m%d%H%M")
         }
-      elsif period == "until_one_year"
-        params = {
-          date_to: datetime.ago(1.years).strftime("%Y%m%d%H%M"),
+      when period == "until_one_year"
+        {
+          date_to: datetime.ago(1.year).strftime("%Y%m%d%H%M"),
           date_from: datetime.ago(10.years).strftime("%Y%m%d%H%M")
         }
       end
     end
-        
-    #クエリ指定
+
+    # クエリ指定
     def fetch_request_query(query_params)
-        body = "{
+      "{
           \"query\":\"from:#{query_params[:login_user]}\",
           \"fromDate\":\"#{query_params[:date_from]}\",
           \"toDate\":\"#{query_params[:date_to]}\"
-        }"      
+      }"
     end
   end
 end
--- spec/support/omniauth_mocks.rb
+++ spec/support/omniauth_mocks.rb
@@ -1,30 +1,30 @@
 module OmniauthMocks
   def twitter_mock_uid_234567
     OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new({
-      'provider' => 'twitter',
-      'uid' => '234567',
-      'info' => {
-        'nickname' => 'mockmock',
-        'name' => 'Mock User',
-        'image' => 'http://mock_image_url.com',
-        'location' => '',
-        'email' => 'mock@example.com',
-        'urls' => {
-          'Twitter' => 'https://twitter.com/MockUser1234',
-          'Website' => ''
+      "provider" => "twitter",
+      "uid" => "234567",
+      "info" => {
+        "nickname" => "mockmock",
+        "name" => "Mock User",
+        "image" => "http://mock_image_url.com",
+        "location" => "",
+        "email" => "mock@example.com",
+        "urls" => {
+          "Twitter" => "https://twitter.com/MockUser1234",
+          "Website" => ""
         }
       },
-      'credentials' => {
-        'token' => 'mock_credentails_token',
-        'secret' => 'mock_credentails_secret'
+      "credentials" => {
+        "token" => "mock_credentails_token",
+        "secret" => "mock_credentails_secret"
       },
-      'extra' => {
-        'raw_info' => {
-          'name' => 'Mock User',
-          'id' => '123456',
-          'followers_count' => 0,
-          'friends_count' => 0,
-          'statuses_count' => 0
+      "extra" => {
+        "raw_info" => {
+          "name" => "Mock User",
+          "id" => "123456",
+          "followers_count" => 0,
+          "friends_count" => 0,
+          "statuses_count" => 0
         }
       }
     })
@@ -32,30 +32,30 @@ module OmniauthMocks
 
   def twitter_mock_uid_123456
     OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new({
-      'provider' => 'twitter',
-      'uid' => '123456',
-      'info' => {
-        'nickname' => 'mockmock',
-        'name' => 'Mock User',
-        'image' => 'http://mock_image_url.com',
-        'location' => '',
-        'email' => 'mock@example.com',
-        'urls' => {
-          'Twitter' => 'https://twitter.com/MockUser1234',
-          'Website' => ''
+      "provider" => "twitter",
+      "uid" => "123456",
+      "info" => {
+        "nickname" => "mockmock",
+        "name" => "Mock User",
+        "image" => "http://mock_image_url.com",
+        "location" => "",
+        "email" => "mock@example.com",
+        "urls" => {
+          "Twitter" => "https://twitter.com/MockUser1234",
+          "Website" => ""
         }
       },
-      'credentials' => {
-        'token' => 'mock_credentails_token',
-        'secret' => 'mock_credentails_secret'
+      "credentials" => {
+        "token" => "mock_credentails_token",
+        "secret" => "mock_credentails_secret"
       },
-      'extra' => {
-        'raw_info' => {
-          'name' => 'Mock User',
-          'id' => '123456',
-          'followers_count' => 0,
-          'friends_count' => 0,
-          'statuses_count' => 0
+      "extra" => {
+        "raw_info" => {
+          "name" => "Mock User",
+          "id" => "123456",
+          "followers_count" => 0,
+          "friends_count" => 0,
+          "statuses_count" => 0
         }
       }
     })
@@ -64,29 +64,29 @@ module OmniauthMocks
   # 異常なデータ(uidが欠落)
   def twitter_invalid_mock
     OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new({
-      'provider' => 'twitter',
+      "provider" => "twitter",
       # "uid" => "123456",
-      'info' => {
-        'name' => 'Mock User',
-        'image' => 'http://mock_image_url.com',
-        'location' => '',
-        'email' => 'mock@example.com',
-        'urls' => {
-          'Twitter' => 'https://twitter.com/MockUser1234',
-          'Website' => ''
+      "info" => {
+        "name" => "Mock User",
+        "image" => "http://mock_image_url.com",
+        "location" => "",
+        "email" => "mock@example.com",
+        "urls" => {
+          "Twitter" => "https://twitter.com/MockUser1234",
+          "Website" => ""
         }
       },
-      'credentials' => {
-        'token' => 'mock_credentails_token',
-        'secret' => 'mock_credentails_secret'
+      "credentials" => {
+        "token" => "mock_credentails_token",
+        "secret" => "mock_credentails_secret"
       },
-      'extra' => {
-        'raw_info' => {
-          'name' => 'Mock User',
-          'id' => '123456',
-          'followers_count' => 0,
-          'friends_count' => 0,
-          'statuses_count' => 0
+      "extra" => {
+        "raw_info" => {
+          "name" => "Mock User",
+          "id" => "123456",
+          "followers_count" => 0,
+          "friends_count" => 0,
+          "statuses_count" => 0
         }
       }
     })
