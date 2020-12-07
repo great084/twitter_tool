44252b1169c6053b95c6cc3d14745c7a74224b06
hisadomi <hb.kmaia93@icloud.com>
2020-12-07 23:50:34 +0900

    APIレスポンス取得手法変更

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -9,24 +9,26 @@ class Tweet < ApplicationRecord
   validates :retweet_flag, inclusion: [true, false]
 
   has_many :media, dependent: :destroy
+  require "net/http"
+  require "uri"
+  require "json"
 
   class << self
     def twitter_search_data(query_params)
       uri = URI.parse("https://api.twitter.com/1.1/tweets/search/#{ENV['PLAN']}/#{ENV['LABEL']}.json")
-      req_options = {
-        use_ssl: uri.scheme == "https"
+      headers = { "Authorization": "Bearer #{ENV['BEARER_TOKEN']}", "Content-Type": "application/json" }
+      data = {
+        "query": "from:#{query_params[:login_user]}".to_s,
+        "fromDate": query_params[:date_from].to_s,
+        "toDate": query_params[:date_to].to_s
       }
-      response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
-        http.request(fetch_request_data(uri, query_params))
-      end
-      JSON.parse(response.body)
-    end
 
-    def fetch_request_data(uri, query_params)
-      request = Net::HTTP::Post.new(uri)
-      request["Authorization"] = "Bearer #{ENV['BEARER_TOKEN']}"
-      request.body = fetch_request_query(query_params)
-      request
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = uri.scheme == "https"
+      request = Net::HTTP::Post.new(uri.request_uri, headers)
+      request.body = JSON.generate(data)
+      response = http.request(request)
+      JSON.parse(response.body)
     end
 
     # apiに送るクエリの取得
@@ -42,7 +44,7 @@ class Tweet < ApplicationRecord
       when "until_now"
         {
           date_to: datetime.ago(9.hours).strftime("%Y%m%d%H%M"),
-          date_from: datetime.ago(1.year).strftime("%Y%m%d%H%M")
+          date_from: datetime.ago(28.days).strftime("%Y%m%d%H%M")
         }
       when period == "until_one_year"
         {
@@ -52,13 +54,13 @@ class Tweet < ApplicationRecord
       end
     end
 
-    # クエリ指定
-    def fetch_request_query(query_params)
-      "{
-          \"query\":\"from:#{query_params[:login_user]}\",
-          \"fromDate\":\"#{query_params[:date_from]}\",
-          \"toDate\":\"#{query_params[:date_to]}\"
-      }"
+    def sent_query(query_params)
+      data = {
+        "query": "from:#{query_params[:login_user]}".to_s,
+        "fromDate": query_params[:date_from].to_s,
+        "toDate": query_params[:date_to].to_s
+      }
+      data.merge({ "next": query_params[:next].to_s }) if query_params[:next]
     end
   end
 end
