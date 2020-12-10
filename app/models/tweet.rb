aa9b491857589a94eecacceb3fd7668dbd5d737f
hisadomi <hb.kmaia93@icloud.com>
2020-12-10 18:45:18 +0900

    期間日数指定修正

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -33,7 +33,7 @@ class Tweet < ApplicationRecord
       when "until_now"
         {
           date_to: datetime.strftime("%Y%m%d%H%M"),
-          date_from: datetime.ago(28.days).strftime("%Y%m%d%H%M")
+          date_from: datetime.ago(1.year).strftime("%Y%m%d%H%M")
         }
       when period == "until_one_year"
         {
