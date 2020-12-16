637764973589912154dd44db01021613f69b7dbd
hisadomi <hb.kmaia93@icloud.com>
2020-12-17 08:41:49 +0900

    リツイート確認表示、リツイート済みフラッグ変更

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -49,6 +49,7 @@ class TweetsController < ApplicationController
     tweet = params_retweet
     client = Tweet.twitter_client(current_user)
     client.retweet(tweet.tweet_id.to_i)
+    tweet.update(retweet_flag: true)
     redirect_to tweet_path(tweet), success: "リツイートに成功しました"
   rescue StandardError => e
     redirect_to tweets_path, danger: "リツイートに失敗しました #{e}"
