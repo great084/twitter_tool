8e2594501d151ea6dc3c615e3fd2138b17de34fa
great084 <great084@gmail.com>
2020-12-18 15:13:20 +0900

    Basic認証の設定追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
+  if Rails.env.production?
+    http_basic_authenticate_with name: ENV["BASIC_AUTH_USERNAME"],
+                                 password: ENV["BASIC_AUTH_PASSWORD"]
+  end
 end
