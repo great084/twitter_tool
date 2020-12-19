cf25e8a5b26f68ee56c03c4657b7c4adc9edb1bb
hisadomi <hb.kmaia93@icloud.com>
2020-12-20 03:08:59 +0900

    ツイート取得時のテーブル格納処理をhelperへ移行

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,6 @@
 class TweetsController < ApplicationController
   include SessionsHelper
+  include TweetsHelper
   before_action :date_params_check, only: [:search]
   before_action :tweet_user, only: %i[show index search retweet]
   PER_PAGE = 10
@@ -31,18 +32,6 @@ class TweetsController < ApplicationController
     redirect_to tweets_path
   end
 
-  def create_records(response)
-    response["results"].each do |res|
-      tweet = Tweet.find_by(tweet_id: res["id_str"])
-      if tweet
-        update_tweet_record(tweet, res)
-      else
-        create_tweet_record(res)
-        extended_entities_exist(res["extended_entities"])
-      end
-    end
-  end
-
   def date_params_check
     return if params.permit(:period).present?
 
@@ -64,39 +53,6 @@ class TweetsController < ApplicationController
     def form_params
       params.permit(:period)
             .merge(login_user: current_user.nickname)
-    end
-
-    def create_tweet_record(res)
-      Tweet.create!(
-        user_id: current_user.id,
-        tweet_created_at: res["created_at"],
-        tweet_id: res["id_str"],
-        text: res["text"],
-        retweet_count: res["retweet_count"],
-        favorite_count: res["favorite_count"]
-      )
-    end
-
-    def update_tweet_record(tweet, res)
-      tweet.update(
-        retweet_count: res["retweet_count"],
-        favorite_count: res["favorite_count"]
-      )
-    end
-
-    def extended_entities_exist(extended_entities)
-      return unless extended_entities
-
-      extended_entities["media"].each do |res_medium|
-        create_medium_record(res_medium)
-      end
-    end
-
-    def create_medium_record(res_medium)
-      Medium.create!(
-        tweet_id: Tweet.last.id,
-        media_url: res_medium["media_url"]
-      )
     end
 
     def tweet_user
