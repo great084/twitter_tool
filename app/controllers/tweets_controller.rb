e64fb42449ffd80930c9793cfebded832102402b
nozomi <kkk@docomo.ne.jp>
2020-12-20 13:44:22 +0900

    ログインユーザーのaccess_tokenとaccesstoken_secretを指定

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -65,6 +65,7 @@ class TweetsController < ApplicationController
         flash[:alert] = "再投稿できませんでした"
         render :post_new
       end
+
   end
 
   private
@@ -131,8 +132,8 @@ class TweetsController < ApplicationController
       @client = Twitter::REST::Client.new do |config|
         config.consumer_key = ENV["TWITTER_API_KEY"]
         config.consumer_secret = ENV["TWITTER_API_SECRET"]
-        config.access_token = ENV["TWITTER_ACCESS_TOKEN"]
-        config.access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
+        config.access_token = current_user.token
+        config.access_token_secret = current_user.secret
       end
     end
 end
