063268c0d7e39fe46ce5e6b004295bdaac9b038d
hisadomi <hb.kmaia93@icloud.com>
2020-12-20 16:27:56 +0900

    rubocop1ファイル行数制限解除、helperからcontrollerへメソッド戻し

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,5 @@
 class TweetsController < ApplicationController
   include SessionsHelper
-  include TweetsHelper
   before_action :date_params_check, only: [:search]
   before_action :tweet_user, only: %i[show index search retweet]
   PER_PAGE = 10
@@ -49,6 +48,51 @@ class TweetsController < ApplicationController
   end
 
   private
+
+    def create_records(response)
+      response["results"].each do |res|
+        tweet = Tweet.find_by(tweet_id: res["id_str"])
+        if tweet
+          update_tweet_record(tweet, res)
+        else
+          create_tweet_record(res)
+          extended_entities_exist(res["extended_entities"])
+        end
+      end
+    end
+
+    def create_tweet_record(res)
+      Tweet.create!(
+        user_id: current_user.id,
+        tweet_created_at: res["created_at"],
+        tweet_id: res["id_str"],
+        text: res["text"],
+        retweet_count: res["retweet_count"],
+        favorite_count: res["favorite_count"]
+      )
+    end
+
+    def update_tweet_record(tweet, res)
+      tweet.update(
+        retweet_count: res["retweet_count"],
+        favorite_count: res["favorite_count"]
+      )
+    end
+
+    def extended_entities_exist(extended_entities)
+      return unless extended_entities
+
+      extended_entities["media"].each do |res_medium|
+        create_medium_record(res_medium)
+      end
+    end
+
+    def create_medium_record(res_medium)
+      Medium.create!(
+        tweet_id: Tweet.last.id,
+        media_url: res_medium["media_url"]
+      )
+    end
 
     def form_params
       params.permit(:period)
