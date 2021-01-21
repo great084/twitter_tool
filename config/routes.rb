b201d09c2732950ba850145cc9246182793fa9d2
nozomi <kkk@docomo.ne.jp>
2021-01-21 19:29:19 +0900

    tweetsに対するルーティングを整理

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,15 @@ Rails.application.routes.draw do
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/auth/failure', to: 'sessions#failure'
   get '/logout', to: 'sessions#destroy'
-  post '/tweets/search', to: 'tweets#search'
+  get '/users', to: 'users#index'
   root to: "tweets#index"
   resources :tweets, only: [:new, :show] do
     member do
       post "post_create"
     end
+    collection do
+      post "search"
+      post "retweet"
+    end
   end
-  post '/tweets/retweet', to: 'tweets#retweet'
-  get '/users', to: 'users#index'
 end
