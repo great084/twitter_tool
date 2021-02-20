f84dc89e4ebd773dc38dd4043b5acbf67ab94d30
nozomi <kkk@docomo.ne.jp>
2021-02-21 07:15:24 +0900

    カラム名修正

--- app/models/auto_tweet.rb
+++ app/models/auto_tweet.rb
@@ -7,7 +7,7 @@ class AutoTweet < ApplicationRecord
   validates :tweet_hour5, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 23 }
   validates :sort_column, presence: true
   validates :order, presence: true
-  validates :exclude_retweet, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :exclude_tweet, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :exclude_repost, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :count, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
